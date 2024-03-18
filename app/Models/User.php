0bdb7e7e03b2b7c26ce4e8ab88b4438fa26e37bb
Maikel <maikel.selder@student.gildeopleidingen.nl>
2024-03-18 11:11:52 +0100

    'laravel 11 init'

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,46 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    protected $guarded = [];
-    public $timestamps = false;
-    protected $table = "user";
+    use HasFactory, Notifiable;
 
-    public function role()
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        return $this->hasOne(UserRole::class, "id", "role_id");
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
-
-
 }
