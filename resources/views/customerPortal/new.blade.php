81c9e994d7c23f107b5f93d95f093b1e4fa713e6
lucas01062003 <lucas.hermus@student.gildeopleidingen.nl>
2024-03-22 09:29:37 +0100

    commit

--- resources/views/customerPortal/new.blade.php
+++ resources/views/customerPortal/new.blade.php
@@ -17,14 +17,18 @@
         <input class="form-input" type="text" id="serienummer" name="serial" placeholder="1234566789"
                data-error-message="Geef een geldig Serienummer op">
         <label class="form-label" for="postcode">Postcode:</label>
-        <input class="form-input" type="text" id="postcode" name="postal_code">
+        <input class="form-input" type="text" id="postcode" name="postal_code" data-error-message="Geef een geldige postcode op.">
         <label class="form-label" for="probleembeschrijving">Probleembeschrijving:</label>
         <textarea class="form-textarea" id="probleembeschrijving" name="description"></textarea>
         <button class="form-button" type="submit">Versturen</button>
+        <br>
+        <br>
         @error('validation')
         <div class="alert alert-danger">Het product met deze postcode kon niet gevonden worden</div>
         @enderror
+
         @if($errors->any() && !$errors->has('validation'))
+
             <div id="form-submit-fail" class="alert alert-danger" role="alert">
                 Reparatieticket aanmaken mislukt. Probeer opnieuw.
             </div>
